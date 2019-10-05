04ed43a70c1648d5d50bd173e962aee3e043b74a
keemo <keemowatson1@gmail.com>
2019-10-05 22:03:50 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,19 +1,13 @@
 # Use this space to code along with the readme
 
-#phrase = "Hello World!"
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-
-def greeting
-  puts "Hello World"
-end
-
-greeting
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
